e6605d0372fc2732adfa7a91a3f314e49fdcefa6
M55 <m55@localhost.localdomain>
2017-08-23 15:47:16 +0300

    1st page

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,36 +1,130 @@
-        <footer class="footer">
-            <div class="container">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="https://www.creative-tim.com">
-                                Creative Tim
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://presentation.creative-tim.com">
-                                About Us
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://blog.creative-tim.com">
-                                Blog
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md">
-                                MIT License
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="copyright">
-                    &copy;
-                    <script>
-                        document.write(new Date().getFullYear())
-                    </script>, Designed by
-                    <a href="http://www.invisionapp.com" target="_blank">Invision</a>. Coded by
-                    <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-                </div>
-            </div>
-        </footer>
+<footer class="footer footer-white footer-big">
+                    <div class="container">
+                        <div class="content">
+                            <div class="row">
+                                <div class="col-md-2 offset-md-1">
+                                    <h5>Legal</h5>
+                                    <ul class="links-vertical">
+                                        <li>
+                                            <a href="#pablo">
+                                                Transactions FAQ
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Terms &amp; Conditions
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Licenses
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-2">
+                                    <h5>Legal</h5>
+                                    <ul class="links-vertical">
+                                        <li>
+                                            <a href="#pablo">
+                                                Transactions FAQ
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Terms &amp; Conditions
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Licenses
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-2">
+                                    <h5>About</h5>
+                                    <ul class="links-vertical">
+                                        <li>
+                                            <a href="#pablo">
+                                                Blog
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                About Us
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Presentation
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Contact Us
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-2">
+                                    <h5>Market</h5>
+                                    <ul class="links-vertical">
+                                        <li>
+                                            <a href="#pablo">
+                                                Sales FAQ
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                How to Register
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Sell Goods
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Receive Payment
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Transactions Issues
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-2">
+                                    <h5>Legal</h5>
+                                    <ul class="links-vertical">
+                                        <li>
+                                            <a href="#pablo">
+                                                Transactions FAQ
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Terms &amp; Conditions
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#pablo">
+                                                Licenses
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="copyright pull-center">
+                            Все права защищены ©
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script> ООО "Лечебно-диагностическая поликлиника"
+                        </div>
+                    </div>
+                </footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,12 @@
 Эндоскопический центр
 @endsection
 @section('content')
-    <div class="wrapper">
-        <div class="page-header page-header-small header-filter">
-            <div class="page-header-image" data-parallax="true" style="background-image: url(/assets/img/ligator-bgg.jpg) ;">
-            </div>
-                        <div class="container">
-                      <div class="row justify-content-left slider-center">
+<div class="wrapper">
+    <div class="page-header page-header-small header-filter">
+        <div class="page-header-image" data-parallax="true" style="background-image: url(/assets/img/ligator-bgg.jpg) ;">
+        </div>
+        <div class="container">
+          <div class="row justify-content-left slider-center">
             <div class="col-6">
                 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
@@ -51,11 +51,109 @@
                 </div>
             </div>
         </div>  
+    </div>
+</div>
+</div>
+<div class="section">
+    <div class="container">
+        <div class="about-description text-center">
+            <div class="row">
+                <div class="features-3">
+                    <div class="row">
+                        <div class="col-md-8 offset-md-2">
+                            <h2 class="title">Мы предлагаем</h2>
+                            <h4 class="description">Ряд специализированных обследований, которые помогут Вам сохранить здоровье и долголетие!</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">ФГДС</h4>
+                                <p class="description"><b>Фиброэзофагогастродуоденоскопия</b> под видеоконтролем - исследование состояния слизистой оболочки пищевода, желудка и двенадцатиперстной кишки</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">ФКС</h4>
+                                <p class="description"><b>Колоноскопия</b> (исследование толстого кишечника), для выявления различной патологии, включая и онкопатологию</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">УЗИ</h4>
+                                <p class="description">Исследование Вашего организма с использованием высокотехнологичного аппарата при помощи ультразвуковых волн</p>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">Проктология</h4>
+                                <p class="description">Выявление и лечение болезней толстой кишки</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">Гастроэнтерология</h4>
+                                <p class="description">Диагностика и лечение заболеваний желудочно-кишечного тракта</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">Невролог</h4>
+                                <p class="description">Диагностика заболеваний центральной и периферической нервной системы</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-md-offset-2">
+                            <div class="info info-hover">
+                                <h4 class="info-title">Терапевт</h4>
+                                <p class="description">Комплексная диагностика состояния здоровья</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info info-hover">
+                                <h4 class="info-title">Эндокринолог</h4>
+                                <p class="description">Профилактика и лечение патологий эндокринной системы</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="section">
-    </div>
-    @include('includes.footer')
+
+        <div class="about-description text-center">
+                    <div class="row">
+                        <div class="col-md-8 offset-md-2">
+                            <h2 class="title">Как нас найти</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        
+                        <div class="col-md-8 offset-md-2">
+                            <div class="info info-horizontal">
+                                <div class="icon icon-primary">
+                                    <i class="now-ui-icons location_pin"></i>
+                                </div>
+                                <div class="description">
+                                    <h4 class="info-title">Мы находимся по адресу</h4>
+                                    <p> г.Чебоксары, ул. Ярославская, д.72, офис 1 (остановка "Дом торговли")
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+        </div>
+</div>
+<div class="cd-section" id="contactus">
+            <!--     *********    CONTACT US 1     *********      -->
+            <div class="contactus-1 section-image contacts-block" style="background-image: url('assets/img/ldp-map.jpg')">
+              
+            </div>
+            </div>
+            <!--     *********    END CONTACT US 1      *********      -->
+@include('includes.footer')
 </div>
 @endsection
